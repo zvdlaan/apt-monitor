49177d9f8c3d552bd0dc8b8f8977317bfa23bc79
zvdlaan <zach.vanderlaan@ymail.com>
2015-01-19 14:26:51 -0500

    Update temp_model.py

--- models/temp_model.py
+++ models/temp_model.py
@@ -6,25 +6,25 @@ def get_temps_all():
     return db.select('Temperature', what='Id, MONTH(MeasuredTime) AS Month, DAY(MeasuredTime) AS Day, YEAR(MeasuredTime) AS Year, TIME(MeasuredTime) AS Time, UNIX_TIMESTAMP(MeasuredTime) AS Timestamp, InsideTemp, OutsideTemp',  order='Id');
 
 def get_temps_all_stats():
-	return db.query('SELECT COUNT(*) AS NumRowsInQuery, MIN(InsideTemp) AS InsideMin, MAX(InsideTemp) AS InsideMax, AVG(InsideTemp) As InsideMean, STD(InsideTemp) AS InsideStddev FROM Temperature');
+	return db.query('SELECT COUNT(*) AS NumRowsInQuery, MIN(InsideTemp) AS InsideMin, MAX(InsideTemp) AS InsideMax, AVG(InsideTemp) As InsideMean, STD(InsideTemp) AS InsideStddev, MIN(OutsideTemp) AS OutsideMin, MAX(OutsideTemp) AS OutsideMax, AVG(OutsideTemp) As OutsideMean, STD(OutsideTemp) AS OutsideStddev FROM Temperature');
 
 def get_temps_day():
     return db.select('Temperature', what='Id, MONTH(MeasuredTime) AS Month, DAY(MeasuredTime) AS Day, YEAR(MeasuredTime) AS Year, TIME(MeasuredTime) AS Time, UNIX_TIMESTAMP(MeasuredTime) AS Timestamp, InsideTemp, OutsideTemp', where='MeasuredTime >= DATE(NOW())', order='Id');
     
 def get_temps_day_stats():
-	return db.query('SELECT COUNT(*) AS NumRowsInQuery, MIN(InsideTemp) AS InsideMin, MAX(InsideTemp) AS InsideMax, AVG(InsideTemp) As InsideMean, STD(InsideTemp) AS InsideStddev FROM Temperature WHERE MeasuredTime >= DATE(NOW())');    
+	return db.query('SELECT COUNT(*) AS NumRowsInQuery, MIN(InsideTemp) AS InsideMin, MAX(InsideTemp) AS InsideMax, AVG(InsideTemp) As InsideMean, STD(InsideTemp) AS InsideStddev, MIN(OutsideTemp) AS OutsideMin, MAX(OutsideTemp) AS OutsideMax, AVG(OutsideTemp) As OutsideMean, STD(OutsideTemp) AS OutsideStddev FROM Temperature WHERE MeasuredTime >= DATE(NOW())');    
     
 def get_temps_week():
     return db.select('Temperature', what='Id, MONTH(MeasuredTime) AS Month, DAY(MeasuredTime) AS Day, YEAR(MeasuredTime) AS Year, TIME(MeasuredTime) AS Time, UNIX_TIMESTAMP(MeasuredTime) AS Timestamp, InsideTemp, OutsideTemp', where='MeasuredTime >= DATE(NOW()) - INTERVAL 1 WEEK', order='Id');
 
 def get_temps_week_stats():
-	return db.query('SELECT COUNT(*) AS NumRowsInQuery, MIN(InsideTemp) AS InsideMin, MAX(InsideTemp) AS InsideMax, AVG(InsideTemp) As InsideMean, STD(InsideTemp) AS InsideStddev FROM Temperature WHERE MeasuredTime >= DATE(NOW()) - INTERVAL 1 WEEK');    
+	return db.query('SELECT COUNT(*) AS NumRowsInQuery, MIN(InsideTemp) AS InsideMin, MAX(InsideTemp) AS InsideMax, AVG(InsideTemp) As InsideMean, STD(InsideTemp) AS InsideStddev, MIN(OutsideTemp) AS OutsideMin, MAX(OutsideTemp) AS OutsideMax, AVG(OutsideTemp) As OutsideMean, STD(OutsideTemp) AS OutsideStddev FROM Temperature WHERE MeasuredTime >= DATE(NOW()) - INTERVAL 1 WEEK');    
 
 def get_temps_month():
     return db.select('Temperature', what='Id, MONTH(MeasuredTime) AS Month, DAY(MeasuredTime) AS Day, YEAR(MeasuredTime) AS Year, TIME(MeasuredTime) AS Time, UNIX_TIMESTAMP(MeasuredTime) AS Timestamp, InsideTemp, OutsideTemp', where='MeasuredTime >= DATE(NOW()) - INTERVAL 1 MONTH', order='Id');
 
 def get_temps_month_stats():
-	return db.query('SELECT COUNT(*) AS NumRowsInQuery, MIN(InsideTemp) AS InsideMin, MAX(InsideTemp) AS InsideMax, AVG(InsideTemp) As InsideMean, STD(InsideTemp) AS InsideStddev FROM Temperature WHERE MeasuredTime >= DATE(NOW()) - INTERVAL 1 MONTH');    
+	return db.query('SELECT COUNT(*) AS NumRowsInQuery, MIN(InsideTemp) AS InsideMin, MAX(InsideTemp) AS InsideMax, AVG(InsideTemp) As InsideMean, STD(InsideTemp) AS InsideStddev, MIN(OutsideTemp) AS OutsideMin, MAX(OutsideTemp) AS OutsideMax, AVG(OutsideTemp) As OutsideMean, STD(OutsideTemp) AS OutsideStddev FROM Temperature WHERE MeasuredTime >= DATE(NOW()) - INTERVAL 1 MONTH');    
 
 def get_current_temp():
 	return db.select('InstantaneousTemperature', what='InsideTemp' );
