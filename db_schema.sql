f2ecf978c56a61c656be729cfcc2a316627d2b58
zvdlaan <zach.vanderlaan@ymail.com>
2015-01-19 13:42:24 -0500

    Update db_schema.sql

--- db_schema.sql
+++ db_schema.sql
@@ -1,75 +1,26 @@
 
---CREATE DATABASE DB_VandyApt;
+DROP TABLE IF EXISTS Temperature;
+DROP TABLE IF EXISTS InstantaneousTemperature;
 
-DROP TABLE IF EXISTS AmbientRoomTemp;
-DROP TABLE IF EXISTS InstantaneousAmbientRoomTemp;
-
-CREATE TABLE AmbientRoomTemp
+CREATE TABLE Temperature
 (
 	Id INT NOT NULL AUTO_INCREMENT,
 	MeasuredTime DATETIME,
-	Temp DECIMAL(4,1),
+	InsideTemp DECIMAL(4,1),
+	OutsideTemp DECIMAL(4,1),
 	PRIMARY KEY (Id)
 );
 
-CREATE TABLE InstantaneousAmbientRoomTemp
+CREATE TABLE InstantaneousTemperature
 (
 	Id INT NOT NULL AUTO_INCREMENT,
-	Temp DECIMAL(4,1),
+	InsideTemp DECIMAL(4,1),
 	PRIMARY KEY (Id)
 );
 
-INSERT INTO InstantaneousAmbientRoomTemp VALUES (1, 73.2);
+INSERT INTO InstantaneousTemperature VALUES (1, 73.2);
 
--- Day
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-31 00:00:00' , 74 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-31 01:00:00' , 75 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-31 02:00:00' , 76 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-31 03:00:00' , 76 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-31 04:00:00' , 76 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-31 05:00:00' , 76 );
-
-
---Week
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-28 00:00:00' , 71 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-28 01:00:00' , 74 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-28 02:00:00' , 75 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-28 03:00:00' , 72 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-28 04:00:00' , 68 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-28 05:00:00' , 69);
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-28 06:00:00' , 70 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-28 07:00:00' , 71 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-28 08:00:00' , 74 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-28 09:00:00' , 74 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-28 10:00:00' , 75 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-28 11:00:00' , 78 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-28 12:00:00' , 74 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-28 13:00:00' , 80 );
-
-
---Month
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-17 00:00:00' , 71 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-17 01:00:00' , 74 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-17 02:00:00' , 75 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-17 03:00:00' , 72 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-17 04:00:00' , 68 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-17 05:00:00' , 69);
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-17 06:00:00' , 70 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-17 07:00:00' , 71 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-17 08:00:00' , 74 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-17 09:00:00' , 74 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-17 10:00:00' , 75 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-17 11:00:00' , 78 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-17 12:00:00' , 74 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-17 13:00:00' , 80 );
-
-
---Max
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-12-04 00:00:00' , 71 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-11-07 01:00:00' , 74 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-10-08 02:00:00' , 75 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2014-09-04 03:00:00' , 72 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2013-12-10 04:00:00' , 68 );
-INSERT INTO AmbientRoomTemp (MeasuredTime, Temp) VALUES( '2012-01-11 05:00:00' , 69);
+INSERT INTO Temperature (MeasuredTime, Temp) VALUES( '2014-12-31 00:00:00' , 74 );
+INSERT INTO Temperature (MeasuredTime, Temp) VALUES( '2014-12-31 01:00:00' , 75 );
 
 COMMIT;
