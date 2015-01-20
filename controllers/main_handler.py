620e4365db8a86404a8567d569d4c609170cb46a
zvdlaan <zach.vanderlaan@ymail.com>
2015-01-19 23:09:37 -0500

    Update main_handler.py

--- controllers/main_handler.py
+++ controllers/main_handler.py
@@ -100,7 +100,7 @@ class BbControlHandler(object):
 				duty_min = 0
 				duty_max = PWM.GetPeriod('P8_13')
 				duty = (float(data['servo-angle'])/180)*(duty_max-duty_min) + duty_min
-				PWM.SetDuty(duty)
+				PWM.SetDuty('P8_13', duty)
 				returnData['servo-angle'] = 'servo angle set to ' + data['servo-angle']
 			else:
 				returnData['servo-angle'] = 'error: ' + data['servo-angle'] + ' is invalid servo angle' 
